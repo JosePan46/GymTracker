48595660dfb6031d8e424e0291d3fbe6f7027168
JosePan46 <josepan@uma.es>
2022-09-16 21:41:41 +0200

    Initial Commit

---
+++ GymTracker/GymTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackerApp.swift
+//  GymTracker
+//
+//  Created by Jose Pan on 16/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GymTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
